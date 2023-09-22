3b075611d3f9530c84dd1681df8bef4ffae92737
Hawa Ali Drammeh <hawad416@gmail.com>
2023-09-21 23:36:02 -0700

    Delete cypress directory

--- cypress/plugins/index.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const percyHealthCheck = require("@percy/cypress/task");
-
-module.exports = (on) => {
-  on("task", percyHealthCheck);
-};
